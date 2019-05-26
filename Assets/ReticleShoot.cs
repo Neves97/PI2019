eb3b4d6b56764077a9a4d7ec42dfbdc3bc62bd14
Neves97 <jneves97@outlook.com>
2019-05-26 18:33:48 -0300

    Colocado um tiro pra teste, corrigido estabilidade do jogador e agua animadinha :3

---
+++ Assets/ReticleShoot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReticleShoot : MonoBehaviour
+{
+
+    public GameObject bullet;
+    public Transform point;
+    public float force;
+// Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Tiro") ){
+            var clone = Instantiate(bullet, point.position,point.rotation);
+           clone.GetComponent<Rigidbody>().velocity = transform.TransformDirection(Vector3.forward * force);
+
+        }
+    }
+}
